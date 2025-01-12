c7221ddcc31b449acb7cad23ac10e1f6d0da05e3
Jeongbin Yoon <86540140+JeongbinYoon@users.noreply.github.com>
2025-01-12 19:35:11 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a,b,t;
+int main() {
+    scanf("%d",&t);
+    for(int i = 0; i < t; i++){
+        scanf("%d %d", &a, &b);
+        printf("%d\n",a+b);
+    }
+    return 0;
+}
